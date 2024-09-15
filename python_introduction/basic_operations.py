43e89bd0d5de88703086f2b8706ff59aecf62151
Queengeorge <queenkingsleygeorge@gmail.com>
2024-09-15 04:49:39 +0100

    first file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+print( "Addition of", number1, "and", number2, "is", sum)
+subtract = number1 - number2
+print("subtraction of", number1, "and", number2, "is", subtract)
+multiply = number1 * number2
+print("multiplication of", number1, "and", number2, "is", multiply)
