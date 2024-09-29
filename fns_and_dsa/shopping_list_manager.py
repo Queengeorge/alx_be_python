b4f17813b21560d784b567550df7dcb5fb602aad
Queengeorge <queenkingsleygeorge@gmail.com>
2024-09-29 19:57:34 +0100

    new message

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -16,7 +16,7 @@ def main():
 
         if choice == '1':
             
-            item = input("Enter the item you want to add: ").strip()
+            item = input("Enter the item to add: ").strip()
             shopping_list.append(item)
             print(f"'{item}' has been added to your shopping list.")
         
