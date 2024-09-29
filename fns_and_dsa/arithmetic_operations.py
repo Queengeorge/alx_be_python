938ca963512813e4c600b883b542930865a3d224
Queengeorge <queenkingsleygeorge@gmail.com>
2024-09-29 18:42:54 +0100

    new message

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,9 +6,10 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
+     if num2 == 0:
+        return " error: invalid operation" 
         return num1/num2
-    else:
-        return "error: invalid = operation "
+
     
 
 
